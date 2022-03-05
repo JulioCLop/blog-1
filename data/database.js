0d3a72a66b360eddb58dd6f240c5931e11903e4f
Juliolopez <joeyclopez88@gmail.com>
2022-03-04 22:29:47 -0700

    initial commit

---
+++ data/database.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    database: 'blog',
+    user: 'root',
+    password: 'Joey@123'
+});
+
+
+
+module.exports = pool;
